667bad383a8d8d50bdf4244dd058c66f411b16c4
Saba Shavidze <sabashavidze@gmail.com>
2023-06-22 14:21:34 +0400

    NEW: add child assets map to parent: {path:id} format

--- bundler.js
+++ bundler.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
+const path = require("path");
 const babelParser = require("@babel/parser");
 const traverse = require("@babel/traverse").default;
 
 let ID = 0;
 
 function createAsset(filename) {
-  console.log(filename);
   const content = fs.readFileSync(filename, "utf-8");
   const ast = babelParser.parse(content, {
     sourceType: "module",
@@ -13,11 +13,9 @@ function createAsset(filename) {
   const dependencies = [];
   traverse(ast, {
     ImportDeclaration: ({ node }) => {
-      console.log("Node - ", JSON.stringify(node, null, 3));
       dependencies.push(node.source.value);
     },
   });
-  //console.log(dependencies);
   const id = ID++;
   return {
     id,
@@ -29,8 +27,19 @@ function createAsset(filename) {
 function buildDepsTree(entry) {
   const mainAsset = createAsset(entry);
   const queue = [mainAsset];
-  console.log({ queue });
+  queue.forEach((asset) => {
+    const dirname = path.dirname(asset.filename);
+    // save dependencies to its path
+    asset.mapping = {};
+    asset.dependencies.forEach((relativePath) => {
+      const absolutePath = path.join(dirname, relativePath);
+      const childAsset = createAsset(absolutePath);
+      asset.mapping[relativePath] = childAsset.id;
+      queue.push(childAsset);
+    });
+  });
+  return queue;
 }
 
 const tree = buildDepsTree("./src/entry.js");
-console.log(tree);
+console.log(JSON.stringify(tree, null, 3));
